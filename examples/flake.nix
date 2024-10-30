0bbee6409c3e5579a3b02029ff055dea48cb545c
Justus Schock <justus.schock@posteo.de>
2024-10-30 02:51:17 +0100

    update

--- examples/flake.nix
+++ examples/flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "nixpkgs/nixos-24.05";  # Use your desired Nixpkgs channel
     flake-utils.url = "github:numtide/flake-utils";
     justusschock.url = "github:justusschock/nixos-packages";
-    justusschock.flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, justusschock, ... }: 
@@ -13,19 +12,23 @@
         inherit system;
         overlays = [ ];
       };
+      go_1_21_9 = justusschock.packages.${system}.go_1_21_9;
 
-      # Import default.nix from the fetched repo
-      justusschock-packages = import justusschock { inherit pkgs; };
+      allPackages = pkgs.buildEnv {
+        name = "all-packages";
+        paths = pkgs.lib.attrValues (justusschock.packages.${system});
+      };
     in
     {
       packages = {
-        # If your default.nix returns an attribute set of packages
-        go_1_21_9 = justusschock-packages.go_1_21_9;
+        inherit go_1_21_9; 
       };
+      
+      defaultPackage = allPackages;
 
       devShells = {
         default = pkgs.mkShell {
-          buildInputs = [ justusschock-packages.go_1_21_9 ];  # Example usage
+          buildInputs = [ justusschock.go_1_21_9 ];  # Example usage
         };
       };
     }
